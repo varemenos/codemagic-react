5d41bc2a2e03b4633b7593abf7918ebc6a7f439d
Adonis K <aklp08@gmail.com>
2015-07-26 22:35:59 +0300

    added configuration directory

---
+++ app/config/toolbar-items.jsx
@@ -0,0 +1,32 @@
+var toolbarItems = [
+    {
+        type: 'icon',
+        icon: 'html5'
+    },
+    {
+        type: 'icon',
+        icon: 'css3'
+    },
+    {
+        type: 'icon',
+        icon: 'code'
+    },
+    {
+        type: 'icon',
+        icon: 'file'
+    }, {
+        type: 'icon',
+        icon: 'folder'
+    }, {
+        type: 'icon',
+        icon: 'play'
+    }, {
+        type: 'seperator'
+    }, {
+        type: 'icon',
+        icon: 'cog',
+        direction: 'left'
+    }
+];
+
+export default toolbarItems;
