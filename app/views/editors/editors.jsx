0bdfa93db03d88d0d986cafe8d89d56daeae8733
Adonis K <aklp08@gmail.com>
2015-07-29 17:41:31 +0300

    removed some old boilerplate-like code

--- app/views/editors/editors.jsx
+++ app/views/editors/editors.jsx
@@ -4,21 +4,6 @@ import React from 'react';
 import Editor from './editor.jsx';
 
 var Editors = React.createClass({
-    componentDidMount: function () {
-        'use strict';
-
-        var el = this.getDOMNode();
-
-        // el.addEventListener('transitionend', this.transitionEnd);
-    },
-    transitionEnd: function (e) {
-        'use strict';
-
-        // handle transition when .inactive class is added/removed to this DOM node
-
-        var el = this.getDOMNode();
-        console.log(e, el);
-    },
     render: function () {
         'use strict';
 
